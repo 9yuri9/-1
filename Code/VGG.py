b6a9c1865d3f837d4357c294a673eeb7afe45f9e
YURI <ssc2704@gmail.com>
2025-06-12 23:43:19 +0900

    Update VGG.py
    주석추가

--- Code/VGG.py
+++ Code/VGG.py
@@ -1,9 +1,11 @@
 import torch.nn as nn
 from torchvision.models import vgg19, VGG19_Weights
 
+# VGG19 모델 생성 함수
 def get_vgg19(num_classes, pretrained=True):
     weights = VGG19_Weights.DEFAULT if pretrained else None
     model = vgg19(weights=weights)
+    # classifier(FC 블록)를 클래스 수에 맞춰 재정의
     model.classifier = nn.Sequential(
         nn.Linear(25088, 4096),
         nn.ReLU(True),
